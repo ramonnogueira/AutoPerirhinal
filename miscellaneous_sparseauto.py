b150916396b332fd91a133433e1508b1b4b5ed81
ramonnogueira <rnogueiramanas@gmail.com>
2023-03-02 14:08:23 -0500

    added sparsity loss

--- miscellaneous_sparseauto.py
+++ miscellaneous_sparseauto.py
@@ -38,32 +38,47 @@ def classifier(data,clase,reg):
     return np.mean(perf,axis=0)
 
 # Fit the autoencoder. The data needs to be in torch format
-def fit_autoencoder(model,data,n_epochs,batch_size,lr,sigma_noise):
-    train_loader=DataLoader(torch.utils.data.TensorDataset(data,data),batch_size=batch_size,shuffle=True)
+def fit_autoencoder(model,data,clase,n_epochs,batch_size,lr,sigma_noise,beta,beta_sp,p_norm):
+    train_loader=DataLoader(torch.utils.data.TensorDataset(data,data,clase),batch_size=batch_size,shuffle=True)
     optimizer=torch.optim.Adam(model.parameters(), lr=lr)
-    loss=torch.nn.MSELoss()
+    loss1=torch.nn.MSELoss()
+    loss2=torch.nn.CrossEntropyLoss()
     model.train()
-
+    
+    loss_rec_vec=[]
+    loss_ce_vec=[]
+    loss_sp_vec=[]
     loss_vec=[]
     data_epochs=[]
+    data_hidden=[]
     t=0
     while t<n_epochs: 
         #print (t)
         outp=model(data,sigma_noise)
         data_epochs.append(outp[0].detach().numpy())
-        loss_rec=loss(outp[0],data).item()
-        loss_vec.append(loss_rec)
+        data_hidden.append(outp[1].detach().numpy())
+        loss_rec=loss1(outp[0],data).item()
+        loss_ce=loss2(outp[2],clase).item()
+        loss_sp=sparsity_loss(outp[2],p_norm).item()
+        loss_total=((1-beta)*loss_rec+beta*loss_ce+beta_sp*loss_sp)
+        loss_rec_vec.append(loss_rec)
+        loss_ce_vec.append(loss_ce)
+        loss_sp_vec.append(loss_sp)
+        loss_vec.append(loss_total)
         if t==0 or t==(n_epochs-1):
-            print (t,loss_rec)
-        for batch_idx, (targ1, targ2) in enumerate(train_loader):
+            print (t,'rec ',loss_rec,'ce ',loss_ce,'sp ',loss_sp,'total ',loss_total)
+        for batch_idx, (targ1, targ2, cla) in enumerate(train_loader):
             optimizer.zero_grad()
             output=model(targ1,sigma_noise)
-            loss_r=loss(output[0],targ2) # reconstruction error
-            loss_r.backward() # compute gradient
-            optimizer.step() # weigth update
+            loss_r=loss1(output[0],targ2) # reconstruction error
+            loss_cla=loss2(output[2],cla) # cross entropy error
+            loss_s=sparsity_loss(output[2],p_norm)
+            loss_t=((1-beta)*loss_r+beta*loss_cla+beta_sp*loss_s)
+            loss_t.backward() # compute gradient
+            optimizer.step() # weight update
         t=(t+1)
     model.eval()
-    return np.array(loss_vec),np.array(data_epochs)
+    return np.array(loss_rec_vec),np.array(loss_ce_vec),np.array(loss_sp),np.array(loss_vec),np.array(data_epochs),np.array(data_hidden)
 
 # Autoencoder Architecture
 class sparse_autoencoder_1(nn.Module):
@@ -74,6 +89,7 @@ class sparse_autoencoder_1(nn.Module):
         self.sigma_init=sigma_init
         self.enc=torch.nn.Linear(n_inp,n_hidden)
         self.dec=torch.nn.Linear(n_hidden,n_inp)
+        self.dec2=torch.nn.Linear(n_hidden,2)
         self.apply(self._init_weights)
         
     def _init_weights(self, module):
@@ -85,5 +101,14 @@ class sparse_autoencoder_1(nn.Module):
     def forward(self,x,sigma_noise):
         x_hidden = F.relu(self.enc(x))+sigma_noise*torch.randn(x.size(0),self.n_hidden)
         x = self.dec(x_hidden)
-        return x,x_hidden
+        x2 = self.dec2(x_hidden)
+        return x,x_hidden,x2
 
+def sparsity_loss(data,p):
+    #shap=data.size()
+    #nt=shap[0]*shap[1]
+    #loss=(1/nt)*torch.norm(data,p)
+    #loss=torch.norm(data,p)
+    #loss=torch.mean(torch.sigmoid(100*(data-0.1)),axis=(0,1))
+    loss=torch.mean(torch.pow(abs(data),p),axis=(0,1))
+    return loss
--- sparse_autoencoder.py
+++ sparse_autoencoder.py
@@ -37,14 +37,17 @@ path_plots='/home/ramon/Documents/github_repos/AutoEnthorinal/'
 #############################
 # Parameters Training
 #noise during training the autoencoder
-sig_neu=0.1 # noise neurons autoencoder
-sig_inp=1 # noise input
-sig_init=1 #noise weight initialization autoencoder
+sig_neu=0.5 # noise neurons autoencoder
+sig_inp=0.5 # noise input
+sig_init=0.1 #noise weight initialization autoencoder
 n_inp=10
-n_hidden=10 # number hidden units in the autoencoder
+n_hidden=20 # number hidden units in the autoencoder
+beta=1 # between 0 and 1. 0 only reconstruction, 1 only decoding
+beta_sp=1e-10
+p_norm=2
 
 n_trials=100
-n_files=5 # number of files (sessions)
+n_files=2 # number of files (sessions)
 
 batch_size=10 # batch size when fitting network
 lr=1e-3 # learning rate
@@ -56,49 +59,65 @@ x_pre=np.array([[-1,-1],
                 [1,-1],
                 [1,1]])
 
-perf_orig=np.zeros((n_files,2))
-perf_out=np.zeros((n_files,n_epochs,2))
-perf_diff=np.zeros((n_files,n_epochs,2))
-loss_epochs=np.zeros((n_files,n_epochs))
+#perf_orig=np.zeros((n_files,2))
+perf_dire=np.zeros((n_files,n_epochs,2))
+perf_speed=np.zeros((n_files,n_epochs,2))
+perfh_dire=np.zeros((n_files,n_epochs,2))
+perfh_speed=np.zeros((n_files,n_epochs,2))
+loss_epochs=np.zeros((n_files,n_epochs,4))
 for k in range(n_files):
     print (k)
     mat_exp=np.random.normal(0,1/np.sqrt(n_inp),(2,n_inp))
     x_exp=np.dot(x_pre,mat_exp)
     x=np.zeros((len(x_pre)*n_trials,n_inp))
-    clase=np.zeros((len(x_pre)*n_trials,2))
+    clase=np.zeros((len(x_pre)*n_trials,2)) # dim0: direction, dim1: speed
     for i in range(len(x_pre)):
         x[i*n_trials:(i+1)*n_trials]=(np.random.normal(x_exp[i],sig_inp,(n_trials,n_inp)))
         clase[i*n_trials:(i+1)*n_trials]=x_pre[i]
-
-    perf_orig[k]=miscellaneous_sparseauto.classifier(x,clase[:,1],1)
+    clase[clase==-1]=0
+    #perf_orig[k]=miscellaneous_sparseauto.classifier(x,clase[:,1],1)
                                 
     # Fit the autoencoders
     x_torch=Variable(torch.from_numpy(np.array(x,dtype=np.float32)),requires_grad=False)
+    clase_torch=Variable(torch.from_numpy(np.array(clase[:,0],dtype=np.int64)),requires_grad=False) # Only dim0 (direction)
     model=miscellaneous_sparseauto.sparse_autoencoder_1(n_inp=n_inp,n_hidden=n_hidden,sigma_init=sig_init) 
-    loss_vec,data_epochs=miscellaneous_sparseauto.fit_autoencoder(model=model,data=x_torch,n_epochs=n_epochs,batch_size=batch_size,lr=lr,sigma_noise=sig_neu)
-    loss_epochs[k]=loss_vec
+    loss_rec_vec,loss_ce_vec,loss_sp_vec,loss_vec,data_epochs,data_hidden=miscellaneous_sparseauto.fit_autoencoder(model=model,data=x_torch,clase=clase_torch,n_epochs=n_epochs,batch_size=batch_size,lr=lr,sigma_noise=sig_neu,beta=beta,beta_sp=beta_sp,p_norm=p_norm)
+    loss_epochs[k,:,0]=loss_rec_vec
+    loss_epochs[k,:,1]=loss_ce_vec
+    loss_epochs[k,:,2]=loss_sp_vec
+    loss_epochs[k,:,3]=loss_vec
 
     for i in range(n_epochs):
-        perf_out[k,i]=miscellaneous_sparseauto.classifier(data_epochs[i],clase[:,1],1)
-        perf_diff[k,i]=miscellaneous_sparseauto.classifier(x-data_epochs[i],clase[:,1],1)
+        #perf_dire[k,i]=miscellaneous_sparseauto.classifier(x-data_epochs[i],clase[:,0],1) # Decode direction
+        #perf_speed[k,i]=miscellaneous_sparseauto.classifier(x-data_epochs[i],clase[:,1],1) # Decode Speed
+        perf_dire[k,i]=miscellaneous_sparseauto.classifier(data_epochs[i],clase[:,0],1) # Decode direction
+        perf_speed[k,i]=miscellaneous_sparseauto.classifier(data_epochs[i],clase[:,1],1) # Decode Speed
+        perfh_dire[k,i]=miscellaneous_sparseauto.classifier(data_hidden[i],clase[:,0],1) # Decode direction
+        perfh_speed[k,i]=miscellaneous_sparseauto.classifier(data_hidden[i],clase[:,1],1) # Decode Speed
 
 # Plot Loss
 loss_m=np.mean(loss_epochs,axis=0)
-plt.plot(loss_m)
+plt.plot(loss_m[:,0],color='blue',label='Reconstr.')
+plt.plot(loss_m[:,1],color='red',label='Class.')
+plt.plot(loss_m[:,2],color='green',label='Sparsity')
+plt.plot(loss_m[:,3],color='black',label='Total')
 plt.ylabel('Training Loss')
 plt.xlabel('Epochs')
+plt.legend(loc='best')
 plt.show()
         
 # Plot performance
-perf_m=np.mean(perf_orig,axis=0)
-perf_out_m=np.mean(perf_out,axis=0)
-perf_diff_m=np.mean(perf_diff,axis=0)
+#perf_m=np.mean(perf_orig,axis=0)
+perf_dire_m=np.mean(perf_dire,axis=0)
+perf_speed_m=np.mean(perf_speed,axis=0)
+perfh_dire_m=np.mean(perfh_dire,axis=0)
+perfh_speed_m=np.mean(perfh_speed,axis=0)
 
-plt.plot(perf_out_m[:,1],color='blue',label='Out')
-plt.plot(perf_diff_m[:,1],color='red',label='Diff')
-plt.plot(perf_m[1]*np.ones(n_epochs),color='grey',label='Input')
-#plt.plot(perf_out_m[:,0],color='blue',linestyle='--')
-#plt.plot(perf_diff_m[:,0],color='red',linestyle='--')
+plt.plot(perf_dire_m[:,1],color='red',label='Direction')
+plt.plot(perf_speed_m[:,1],color='blue',label='Speed')
+#plt.plot(perf_m[1]*np.ones(n_epochs),color='grey',label='Input')
+plt.plot(perfh_dire_m[:,1],color='red',linestyle='--',label='Dire H')
+plt.plot(perfh_speed_m[:,1],color='blue',linestyle='--',label='Speed H')
 #plt.plot(perf_m[0]*np.ones(n_epochs),color='grey',linestyle='--')
 plt.plot(0.5*np.ones(n_epochs),color='black',linestyle='--')
 plt.ylim([0,1.1])
